d45fe46fde3c5b792d846af3cf9acb6eae7ada08
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2025-08-20 12:22:03 +0530

    change of index.js file envconnect

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
-import express from "express";
 import { configDotenv } from "dotenv";
+configDotenv();
+
+import express from "express";
 import connectDB from "./mongo.config.js";
 import shortenUrl_routes from "./routes/shortUrl.route.js";
 import auth_routes from "./routes/auth.route.js";
-import user_routes from "./routes/user.route.js"
+import user_routes from "./routes/user.route.js";
 import { redirectFromShortUrl } from "./controllers/shortUrl.controller.js";
 import { errorHandler } from "./utils/errorHandler.util.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-configDotenv();
+
 app.use(cookieParser());
 app.use(cors({
   origin: true,
   credentials: true
 }));
-
 app.use(express.json());
 
 app.listen(8080, () => {
@@ -26,8 +27,7 @@ app.listen(8080, () => {
 
 app.use("/api/create", shortenUrl_routes);
 app.use("/api/auth", auth_routes);
-app.use("/api/user",user_routes)
-
+app.use("/api/user", user_routes);
 
 app.get("/:id", redirectFromShortUrl);
 
